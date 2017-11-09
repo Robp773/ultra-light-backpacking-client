248a192af67af3ea1d2a0ab54cb9f864be56adcd
Your Name <robpeterman573@gmail.com>
2017-11-09 17:19:02 -0500

    set up Home component as root

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default class Home extends React.Component{
+super(props){
+    constructor(props);
+}
+
+render(){
+    return(
+        <div>Test</div>
+    )
+}
+}
