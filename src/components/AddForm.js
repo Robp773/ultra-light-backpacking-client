408dc788053dfce62ef4579f1b85705a38500c51
Your Name <robpeterman573@gmail.com>
2017-11-13 13:55:06 -0500

    added legend to add form

--- src/components/AddForm.js
+++ src/components/AddForm.js
@@ -3,8 +3,9 @@ import './AddForm.css'
 export default function AddForm(props){
     return(
         <form>
-            <input name='name' type='text' placeholder='Item Name'/>
-            <input name='weight' type='text' placeholder='Item Weight'/>
+            <legend>Add Item</legend>
+            <input className='addInput' name='name' type='text' placeholder='Item Name'/>
+            <input className='addInput' name='weight' type='text' placeholder='Item Weight'/>
             <button type='submit'>Submit</button>
         </form>
     )
