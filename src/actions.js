d6fb9f853929f3d91fda5d336852a69ad92b1b7f
Your Name <robpeterman573@gmail.com>
2017-11-22 17:58:01 -0500

    updateItem and updateGoal actions

--- src/actions.js
+++ src/actions.js
@@ -5,3 +5,20 @@ export const addItem = (name, weight, title) =>({
     weight: weight,
     title: title
 })
+export const updateItem = (itemState, index, title)=>({
+    type: 'UPDATE_ITEM',
+    name: itemState.name,
+    weight: itemState.weight,
+    title: title,
+    index: index
+})
+export const deleteItem = (title, name, weight) =>({
+    type: 'DELETE_ITEM',
+    title: title,
+    name: name,
+    weight: weight
+})
+export const updateGoal = (value) =>({
+type: 'UPDATE_GOAL',
+value: value
+})
