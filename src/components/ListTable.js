fbb798de57333ac55bf25858969df49b985e1d7f
Your Name <robpeterman573@gmail.com>
2017-12-01 18:50:55 -0500

    removed reduxForm

--- src/components/ListTable.js
+++ src/components/ListTable.js
@@ -1,27 +1,16 @@
 import React from 'react';
 import './ListTable.css';
-import {reduxForm, Field} from 'redux-form';
-
 export default class ListTable extends React.Component{
 
 render(){
-
         return (
         <div className='scrollableParent'>
                     <h4>Name</h4>
                     <h4>Weight</h4>
             <form>
-                    
-                    {this.props.displayItems}
-                                        
+                    {this.props.displayItems}                                   
                     </form>
-              
-              
-                
-                
         </div>
     )
 }
-
 }
-// export default reduxForm({form: 'listTable'})(ListTable);
