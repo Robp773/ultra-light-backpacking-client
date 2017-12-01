6c92fc70835ab183af2e3c637f52db0af48b9191
Your Name <robpeterman573@gmail.com>
2017-12-01 18:49:18 -0500

    limited weight total to two decimal places

--- src/components/CategoryTotal.js
+++ src/components/CategoryTotal.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import './CategoryTotal.css';
-import {connect} from 'react-redux';
-import {updateTotals} from '../actions';
 export default class CategoryTotal extends React.Component{
-
-
+constructor(props){
+    super(props);
+}
 render(){
-
+    console.log('categoryTotal render')
     return(
     <div className='topCategory'>
     <h3>{this.props.title}</h3>
     <div className='itemTotal'>{this.props.itemTotal} items</div> 
-    <div className='weightTotal'>{this.props.weightTotal} ounces</div> 
+    <div className='weightTotal'>{this.props.weightTotal.toFixed(2)} ounces</div> 
     </div>
     )
 }
 }
+
