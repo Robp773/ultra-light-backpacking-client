af5ad3d18f5ce76250d1aeb4c4101fc9850d2c82
Your Name <robpeterman573@gmail.com>
2017-12-03 18:52:30 -0500

    changed delete action to only use index +  added state initialization action

--- src/actions.js
+++ src/actions.js
@@ -1,24 +1,27 @@
-
 export const addItem = (name, weight, title) =>({
     type: 'ADD_ITEM',
     name: name,
     weight: weight,
     title: title
-})
+});
 export const updateItem = (itemState, index, title)=>({
     type: 'UPDATE_ITEM',
     name: itemState.name,
     weight: itemState.weight,
     title: title,
     index: index
-})
-export const deleteItem = (title, name, weight) =>({
+});
+export const deleteItem = (title, index) =>({
     type: 'DELETE_ITEM',
     title: title,
-    name: name,
-    weight: weight
-})
+    index: index
+});
 export const updateGoal = (value) =>({
-type: 'UPDATE_GOAL',
-value: value
+    type: 'UPDATE_GOAL',
+    value: value
+});
+
+export const setListState = (listState)=>({
+    type: 'SET_STATE',
+    state: listState
 })
