b0cd282188383bdd2d1b5b59f03ce18997001f75
Your Name <robpeterman573@gmail.com>
2017-12-03 18:57:03 -0500

    added state init action and fetch req for a user selected list

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,27 +1,51 @@
 import React from 'react';
-import SelectList from './SelectList'
-import ListHome from './ListHome'
-export default class Main extends React.Component{
+import SelectList from './SelectList';
+import ListHome from './ListHome';
+import {API_BASE_URL} from '../config';
+import {connect} from 'react-redux';
+import {setListState} from '../actions.js'
+
+export class Main extends React.Component{
     constructor(props){
-        super(props);
+         super(props);
         this.state = {
-            selectionMade: true,
+            selectionMade: false,
             // signedIn: true
         }
         this.handleChoice = this.handleChoice.bind(this);
     }
-    handleChoice(){
+
+    handleChoice(listName){
+        fetch(`${API_BASE_URL}/list-state/${listName.listName}`)
+        .then((res)=>{
+            res.json()
+            .then((resJSON)=>{
+                this.props.dispatch(setListState(resJSON))
+            })
+        })
         this.setState({selectionMade: true})
     }
+    componentWillMount(){
+        fetch(`${API_BASE_URL}/list-state/name-list`)
+        .then((res)=>{
+            res.json()
+            .then((resJSON)=>{
+              const listNameDivs = resJSON.map((listName, index)=>{
+                    return (<div className='listNameDiv' key={index} onClick={()=>{this.handleChoice({listName})}}>{listName}</div>)
+                });
+                this.setState({listNames: listNameDivs});
+            })
+        })
+    };
+
     render(){
-        // if the user has signed in, bring up a list of that user's packing lists
-        if(this.state.signedIn){
-            return (<SelectList onChoice={this.handleChoice}/>)
-        }
-       else if(this.state.selectionMade)
-            return (
-                <ListHome/>
-            )
-        
+
+            if(this.state.selectionMade) return (<ListHome />)
+
+            else {
+                    return <SelectList listNames={this.state.listNames}/>
+                 }
+            } 
     }
-}
+
+export default connect()(Main);
