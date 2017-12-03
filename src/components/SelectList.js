9ab581a6eedcdc7ebbafb6c4db3c55ab24b8dc52
Your Name <robpeterman573@gmail.com>
2017-12-03 18:58:02 -0500

    population of component with list of listnames

--- src/components/SelectList.js
+++ src/components/SelectList.js
@@ -1,20 +1,24 @@
 
 import React from 'react';
-import './SelectList.css'
-import {API_BASE_URL} from '../config'
+import './SelectList.css';
+import {API_BASE_URL} from '../config';
+
 export default class SelectList extends React.Component{
     constructor(props){
         super(props);
-    this.nameArray;
     }
 
+    // List of category titles should return an array
+
     render(){
+       
         return (
             <div className='listBody'>
             <h3>Select a List</h3>
-                <div className='listItemsParent'>
+                <div className='listNamesParent' >
+                {this.props.listNames}
                 </div>
             </div>
         )
-    }
-}
+    };
+};
