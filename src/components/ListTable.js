6bf6ebb697e3d6c89986421ffb6398f7d006d19a
Your Name <robpeterman573@gmail.com>
2017-12-07 23:43:40 -0500

    removed name and weight headers

--- src/components/ListTable.js
+++ src/components/ListTable.js
@@ -5,11 +5,9 @@ export default class ListTable extends React.Component{
 render(){
         return (
         <div className='scrollableParent'>
-                    <h4>Name</h4>
-                    <h4>Weight(ozs)</h4>
             <form>
-                    {this.props.displayItems}                                   
-                    </form>
+              {this.props.displayItems}                                   
+            </form>
         </div>
     )
 }
