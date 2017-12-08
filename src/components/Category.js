cd9899eaac469706d81c11e8c1f6ccdb2f391251
Your Name <robpeterman573@gmail.com>
2017-12-07 23:22:17 -0500

    added notification modals + font awesome icons + suggestion to add items to list if empty

--- src/components/Category.js
+++ src/components/Category.js
@@ -5,36 +5,48 @@ import ListTable from './ListTable';
 import CategoryTotal from './CategoryTotal';
 import {connect} from 'react-redux';
 import {updateItem, deleteItem} from '../actions';
+import {NotificationManager} from 'react-notifications';
 
 export class Category extends React.Component{
+
         
     state ={
     selected: false,
     formOpen: false,
+    change: false
 } 
 
 
 handleChange(values, index){
     this.props.dispatch(updateItem(values, index, this.props.title.toLowerCase()))
+    this.setState({change: !this.state.change})    
 };
 
 handleDeleteClick(index){
+    NotificationManager.warning('Item Deleted')
     this.props.dispatch(deleteItem(this.props.title.toLowerCase(), index))
+    this.setState({change: !this.state.change})
 };
 
 render(){
-
+    
     let weightTotal = 0;
-    let buttonSymbol, addForm;
+    let buttonSymbol, addForm, displayItems;
     
     if(this.state.formOpen === true){
-     buttonSymbol = '-';
+     buttonSymbol = <i className="fa fa-minus  addOrSub" aria-hidden="true"></i>
+     ;
      addForm = <AddForm title = {this.props.title}/>
     }
     else{
-        buttonSymbol = '+'
+        buttonSymbol = <i className="fa fa-plus addOrSub" aria-hidden="true"></i>
+        
     }  
-    const displayItems = this.props.thisState.map((item, index)=>{       
+    if(this.props.thisState.length === 0){
+          displayItems = <div><div>This list needs some items...</div></div>
+    }
+     else{
+          displayItems = this.props.thisState.map((item, index)=>{    
         
         weightTotal += item.weight;
                  const name = 'name'+ index;
@@ -45,11 +57,11 @@ render(){
         <fieldset key={index} onChange={()=>{
         this.handleChange({name: this[name].value, weight: this[weight].value}, index)}}>
 
-            <input ref={(input)=>{this[name] = input}} type='text' onClick={(e)=>{e.stopPropagation(); 
-        }}defaultValue={item.name} className='indivResult'/>
+        <input ref={(input)=>{this[name] = input}} type='text' onClick={(e)=>{e.stopPropagation(); 
+        }}defaultValue={item.name} className='indivResult name'/>
       
         <input ref={(input)=>{this[weight] = input}} type='text' onClick={(e)=>{e.stopPropagation();
-        }}defaultValue={item.weight} className='indivResult' />
+        }}defaultValue={item.weight} className='indivResult weight' />
 
             <button className='deleteBtn' onClick={(e)=>{e.stopPropagation(); e.preventDefault()}}>
                 <i onClick={()=>this.handleDeleteClick(index)} className="fa fa-trash" aria-hidden="true"></i>
@@ -60,6 +72,7 @@ render(){
 
        )
     })
+}
    
     const loadedCategory = <CategoryTotal title= {this.props.title} 
                             itemTotal={this.props.thisState.length} 
