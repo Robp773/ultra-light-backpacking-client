29912d6c1728ff24375940cd01205fdb72a0ea12
Your Name <robpeterman573@gmail.com>
2017-12-07 23:26:30 -0500

    added input for adding new names +  backpack img

--- src/components/SelectList.js
+++ src/components/SelectList.js
@@ -5,12 +5,18 @@ export default class SelectList extends React.Component{
 
     render(){
        
-        return (
+        return (            
+            <div>
+        <h1>Pack Light</h1>
+
             <div className='listBody'>
+            <img alt='Backpack icon' src='..\images\backpack.png'/>
             <h3>Select a List</h3>
-                <div className='listNamesParent' >
-                {this.props.listNames}
+                <div className='listNamesParent' >                
+                    {this.props.newListInput}
+                    {this.props.listNames}
                 </div>
+            </div>
             </div>
         )
     };
