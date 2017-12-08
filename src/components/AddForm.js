30a056f134385d087f457a0016040abeb8db555e
Your Name <robpeterman573@gmail.com>
2017-12-07 23:19:57 -0500

    form reset on submit

--- src/components/AddForm.js
+++ src/components/AddForm.js
@@ -6,17 +6,19 @@ export class AddForm extends React.Component{
 
     render(){
     return(
-        <form onClick={(e)=>{e.stopPropagation()}} onSubmit={(e)=>{
+        <form className='addForm' onClick={(e)=>{e.stopPropagation()}} onSubmit={(e)=>{
             e.preventDefault();        
             const name = this.inputName.value
             const weight = Number(this.inputWeight.value)
             const readiedTitle = this.props.title.toLowerCase().replace(/\s+/g, "")
             this.props.dispatch(addItem(name, weight, readiedTitle))
+            this.inputName.value = '';
+            this.inputWeight.value='';
         }}>
             <legend>Add Item</legend>
             <input ref= {(input)=>{this.inputName = input}} className='addInputName' name='name' type='text' placeholder='Item Name'/>
-            <input ref= {(input)=>{this.inputWeight = input}} className='addInputWeight' name='weight' type='number' placeholder='Item Weight'/>
-            <button type='submit' onClick={(e)=>{e.stopPropagation()}}>Submit</button>
+            <input ref= {(input)=>{this.inputWeight = input}} className='addInputWeight' name='weight' type='number' placeholder='ozs'/>
+            <button className='addFormSubmit' type='submit' onClick={(e)=>{e.stopPropagation()}}>Submit</button>
         </form>
     )
 }
