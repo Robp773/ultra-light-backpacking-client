19049e1c934e3268f0d4ae811ed7e37882373b66
Your Name <robpeterman573@gmail.com>
2017-12-15 16:22:33 -0500

    added more responsiveness and visual improvements

--- src/components/CategoryTotal.js
+++ src/components/CategoryTotal.js
@@ -3,12 +3,20 @@ import './CategoryTotal.css';
 export default class CategoryTotal extends React.Component{
 
 render(){
-    return(
+    return(  
+    <div>
+        <div className='bgColorDiv'>
         <div className='topCategory'>
             <h3>{this.props.title}</h3>
             <div className='itemTotal'>{this.props.itemTotal} items</div> 
             <div className='weightTotal'>{this.props.weightTotal.toFixed(2)} ounces</div> 
+        </div>        
+        
+      
+            
+        <img className='categoryIcon' src={this.props.imgSrc}/>
         </div>
+</div>
         )
     }
 }
