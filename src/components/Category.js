98fed1a76b89854c120563b0afde9e14c5d98be3
Your Name <robpeterman573@gmail.com>
2017-12-15 16:23:31 -0500

    moved mapping of state to ListTable component

--- src/components/Category.js
+++ src/components/Category.js
@@ -3,37 +3,31 @@ import './Category.css';
 import AddForm from './AddForm';
 import ListTable from './ListTable';
 import CategoryTotal from './CategoryTotal';
-import {connect} from 'react-redux';
-import {updateItem, deleteItem} from '../actions';
-import {NotificationManager} from 'react-notifications';
 
-export class Category extends React.Component{
+
+export default class Category extends React.Component{
 
     state = {
     // if user clicks on this specific Category component
     selected: false,
     // if user clicks the '+' button to open AddForm component
     formOpen: false,
+    itemTotal: 0,
+    weightTotal: 0
     } 
 
 // if user enters a new value into an existing list item input
-handleChange(values, index){
-    // values refers to the new value of the list item
-    this.props.dispatch(updateItem(values, index, this.props.title.toLowerCase()))
-};
-// if user clicks the delete button on a specific list item
-handleDeleteClick(index){
-    // open alert modal
-    NotificationManager.warning('Item Deleted');    
-    this.props.dispatch(deleteItem(this.props.title.toLowerCase(), index));
 
-};
 
-render(){
-    
-    // counter for this specific category's weight total;
+render(){  
     let weightTotal = 0;
     let buttonSymbol, addForm, displayItems;
+    for(let i=0; i<this.props.thisState.length; i++){
+        weightTotal += this.props.thisState[i].weight;
+    }
+    console.log(weightTotal)
+    // counter for this specific category's weight total;
+  
 // if the form is open change the  form open button symbol from '+' to '-'
     if(this.state.formOpen === true){
      buttonSymbol = <i className="fa fa-minus  addOrSub" aria-hidden="true"></i>;
@@ -45,49 +39,11 @@ render(){
         buttonSymbol = <i className="fa fa-plus addOrSub" aria-hidden="true"></i>
         
     }  
-    // if there is nothing in a category's array, display this message
-    if(this.props.thisState.length === 0){
-          displayItems = <div className='emptyListDiv'><div>This list needs some items...</div></div>
-    }
-    // for each object in this category's array, create a single fieldset
-    // including one input for the name property and one for the weight
-    // delete button for deleting this object from the state
-    else{
 
-        displayItems = this.props.thisState.map((item, index)=>{
-            console.log('mapping');
-            console.log(item.name);
-        // for CategoryTotal component
-        weightTotal += item.weight;
-        // for creating a unique ref for each input depending on it's index
-        const name = 'name'+ index;
-        const weight = 'weight' + index;
 
-       return ( 
-        <fieldset key={index} onChange={()=>{this.handleChange({name: this[name].value, weight: this[weight].value}, index)}}>
-
-            <input ref={(input)=>{this[name] = input}} type='text' 
-                onClick={(e)=>{e.stopPropagation()}}
-                defaultValue={item.name} className='indivResult name'/>
-        
-            <input ref={(input)=>{this[weight] = input}} type='text'
-                onClick={(e)=>{e.stopPropagation()}}
-                defaultValue={item.weight} className='indivResult weight'/>
-
-            <button className='deleteBtn' onClick={(e)=>{
-            e.stopPropagation(); 
-            e.preventDefault()
-            this.handleDeleteClick(index)}}>
-                <i onClick={()=>this.handleDeleteClick(index)} className="fa fa-trash" aria-hidden="true"></i>
-            </button>
-
-        </fieldset>
-       )
-    })
-}
 //   CategoryTotal component with all needed props - used in both conditions
     const loadedCategory = 
-    <CategoryTotal title= {this.props.title} itemTotal={this.props.thisState.length} weightTotal={weightTotal} />
+    <CategoryTotal title= {this.props.title} imgSrc={this.props.imgSrc} itemTotal={this.props.thisState.length} weightTotal={weightTotal} />
     
     // if this category is opened display the CategoryTotal component
     // with the addBtn to let the user open the form
@@ -98,7 +54,7 @@ if(this.state.selected === true){
                 {loadedCategory}
                 <button className='addBtn' onClick={(e)=> {e.stopPropagation(); this.setState({formOpen: !this.state.formOpen})} } >{buttonSymbol}</button>
                 {addForm} 
-                <ListTable className='ListTable' displayItems={displayItems}/>
+                <ListTable className='ListTable' title={this.props.title} thisState={this.props.thisState}/>
             </section>
             )
  }
@@ -113,4 +69,3 @@ if(this.state.selected === true){
     }
 }
 
-export default connect()(Category)
--- src/components/ListTable.js
+++ src/components/ListTable.js
@@ -1,14 +1,73 @@
 import React from 'react';
 import './ListTable.css';
-export default class ListTable extends React.Component{
+import {connect} from 'react-redux';
+import {updateItem, deleteItem} from '../actions';
+import {NotificationManager} from 'react-notifications';
 
-render(){    
+export class ListTable extends React.Component{
+        constructor(props){
+                super(props);
+                this.state = {
+                        change: false
+                }
+        }
+
+            // if there is nothing in a category's array, display this message
+
+    // for each object in this category's array, create a single fieldset
+    // including one input for the name property and one for the weight
+    // delete button for deleting this object from the state
+
+handleChange(values, index){     
+        // values refers to the new value of the list item
+        this.props.dispatch(updateItem(values, index, this.props.title.toLowerCase()))};
+        // if user clicks the delete button on a specific list item
+
+handleDeleteClick(index){
+        // open alert modal
+        NotificationManager.warning('Item Deleted');    
+        this.props.dispatch(deleteItem(this.props.title.toLowerCase(), index))};
+
+render(){   
+        let displayItems;
+       
+        displayItems = this.props.thisState.map((item, index)=>{
+      
+        // for CategoryTotal component
+        // for creating a unique ref for each input depending on it's index
+        const name = 'name'+ index;
+        const weight = 'weight' + index;
+       
+       return ( 
+        <fieldset key={index} onChange={()=>{this.handleChange({name: this[name].value, weight: this[weight].value}, index)}}>
+
+            <input ref={(input)=>{this[name] = input}} type='text' 
+                onClick={(e)=>{e.stopPropagation()}}
+                value={item.name} onChange={()=>{this.setState({change: !this.state.change})}}/>
+        
+            <input ref={(input)=>{this[weight] = input}} type='number'
+                onClick={(e)=>{e.stopPropagation()}}
+                value={item.weight} className='indivResult weight' onChange={()=>{this.setState({change: !this.state.change})}}/>
+
+            <button 
+            className='deleteBtn' onClick={(e)=>{e.stopPropagation(); e.preventDefault(); 
+                this.handleDeleteClick(index)}}>
+                <i className="fa fa-trash" aria-hidden="true"></i>
+            </button>
+
+        </fieldset>
+       )
+    })  
+    if(this.props.thisState.length === 0){
+        displayItems = <div className='emptyListDiv'><div>This list needs some items...</div></div>
+  }  
         return (
                 <div className='scrollableParent'>
                         <form className='listItemForm'>
-                            {this.props.displayItems}                                   
+                            {displayItems}
                         </form>
                 </div>
                )
         }
 }
+export default connect()(ListTable)
