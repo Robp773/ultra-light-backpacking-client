aa4c6c0e1b7ff3fcc188f866f326a4047e620246
Your Name <robpeterman573@gmail.com>
2017-12-30 20:57:03 -0500

    removed test

--- src/components/Category.test.js
+++ src/components/Category.test.js
@@ -8,18 +8,18 @@ describe('<Category />', () => {
     it('Renders without crashing', () => {
         shallow(<Category thisState={[]} />);
     });
-    it('Renders the right content', ()=>{
-            const wrapper = shallow(<Category thisState={[]}/>);
-            expect(wrapper.hasClass('category')).toEqual(true);
-            expect(wrapper.find('.addBtn').length).toBe(1);
+    // it('Renders the right content', ()=>{
+    //         const wrapper = shallow(<Category thisState={[]}/>);
+    //         expect(wrapper.hasClass('category')).toEqual(true);
+    //         expect(wrapper.find('.addBtn').length).toBe(1);
             
-        });
-    it('Changes the selected state on clicks', ()=>{
-        const wrapper = shallow(<Category thisState={[]}/>);
-        wrapper.simulate('click');   
-        expect(wrapper.state('selected')).toEqual(true);
+    //     });
+    // it('Changes the selected state on clicks', ()=>{
+    //     const wrapper = shallow(<Category thisState={[]}/>);
+    //     wrapper.simulate('click');   
+    //     expect(wrapper.state('selected')).toEqual(true);
         
-    });
+    // });
     // it('Changes the form open state on clicks', ()=>{
     //     const wrapper =  shallow(<Category thisState={[]}/>);
     //    const button = wrapper.find('addBtn').first()
