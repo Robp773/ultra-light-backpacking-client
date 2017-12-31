71ad674957985a985899939ff41340666921906a
Your Name <robpeterman573@gmail.com>
2017-12-30 20:25:17 -0500

    fixed netlify deployment bug

--- src/config.js
+++ src/config.js
@@ -1,8 +1,14 @@
 'use strict';
 
+let API_BASE_URL; 
+if(process.env.NODE_ENV==='production') { 
+  API_BASE_URL= 'https://lit-hamlet-54666.herokuapp.com';
+} else {  
+  API_BASE_URL='http://localhost:8080';
+} 
+
 module.exports = {
   PORT: process.env.PORT || 8080,
   // other stuff
-  API_BASE_URL: process.env.REACT_APP_API_BASE_URL ||
-      'http://localhost:3000/api'
-};
+  API_BASE_URL: API_BASE_URL
+}; 
