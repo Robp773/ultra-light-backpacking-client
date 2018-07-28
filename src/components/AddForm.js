99dc5a543022970334e8bf8d68f9f4aed4bf6269
Robp773 <robpeterman573@gmail.com>
2018-07-28 13:06:22 -0400

    added functionality for item 'importance' input

--- src/components/AddForm.js
+++ src/components/AddForm.js
@@ -1,30 +1,38 @@
 import React from 'react';
 import './AddForm.css'
-import {connect} from 'react-redux';
-import {addItem} from '../actions'
-export class AddForm extends React.Component{
+import { connect } from 'react-redux';
+import { addItem } from '../actions'
+export class AddForm extends React.Component {
 
-    onSubmit(e){
-        e.preventDefault();        
-        const name = this.inputName.value
-        const weight = Number(this.inputWeight.value)
+    onSubmit(e) {
+        e.preventDefault();
+        const name = this.inputName.value;
+        const weight = Number(this.inputWeight.value);
+        const importance = this.inputImportance.value;
         // filtered title so it can be used to match with state properties 
         // ex. - First Aid = firstaid
-        const readiedTitle = this.props.title.toLowerCase().replace(/\s+/g, "")
-        this.props.dispatch(addItem(name, weight, readiedTitle))
+        const readiedTitle = this.props.title.toLowerCase().replace(/\s+/g, "");
+        this.props.dispatch(addItem(name, weight, importance, readiedTitle));
         // reset form inputs
         this.inputName.value = '';
-        this.inputWeight.value='';
+        this.inputWeight.value = '';
     }
 
-    render(){
-        return(
-            <form className='addForm' 
-                onClick={(e)=>{e.stopPropagation()}} 
-                onSubmit={(e)=>{this.onSubmit(e)}}>
-                <input ref= {(input)=>{this.inputName = input}} className='addInputName' name='name' type='text' placeholder='New Item Name'/>
-                <input ref= {(input)=>{this.inputWeight = input}} className='addInputWeight' name='weight' type='number' placeholder='ozs'/>
-                <button className='addFormSubmit' type='submit' onClick={(e)=>{e.stopPropagation()}}>Submit</button>
+    render() {
+        return (
+            <form className='addForm'
+                onClick={(e) => { e.stopPropagation() }}
+                onSubmit={(e) => { this.onSubmit(e) }}>
+                <input ref={(input) => { this.inputName = input }} className='addInputName' name='name' type='text' placeholder='New Item Name' />
+                <input ref={(input) => { this.inputWeight = input }} className='addInputWeight' name='weight' type='number' placeholder='ozs' />
+                <select ref={(input) => { this.inputImportance = input }} className='addInputImportance' name='importance'>
+                    <option>Importance</option>
+                    <option value='critical'>Critical</option>
+                    <option value='important'>Important</option>
+                    <option value='unimportant'>Unimportant</option>
+                </select>
+
+                <button className='addFormSubmit' type='submit' onClick={(e) => { e.stopPropagation() }}>Submit</button>
             </form>
         )
     }
