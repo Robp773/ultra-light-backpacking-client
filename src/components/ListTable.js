a056cea90f308f02e579028fc28ac262f43a4a46
Robp773 <robpeterman573@gmail.com>
2018-08-01 22:06:30 -0400

    added opacity transition when list items are added or removed + reformatting of code

--- src/components/ListTable.js
+++ src/components/ListTable.js
@@ -1,83 +1,140 @@
-import React from 'react';
-import './ListTable.css';
-import { connect } from 'react-redux';
-import { updateItem, deleteItem } from '../actions';
-import { NotificationManager } from 'react-notifications';
+import React from "react";
+import "./ListTable.css";
+import { connect } from "react-redux";
+import { updateItem, deleteItem } from "../actions";
+import { NotificationManager } from "react-notifications";
+import { CSSTransitionGroup } from "react-transition-group";
 
 export class ListTable extends React.Component {
-        constructor(props) {
-                super(props);
-                this.state = {
-                        change: false
-                }
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      change: false
+    };
+  }
 
-        handleChange(values, index) {
-                // values refers to the new value of the list item
-                this.props.dispatch(updateItem(values, index, this.props.title.toLowerCase()))
-        };
-        // if user clicks the delete button on a specific list item
+  handleChange(values, index) {
+    // values refers to the new value of the list item
+    this.props.dispatch(
+      updateItem(values, index, this.props.title.toLowerCase())
+    );
+  }
+  // if user clicks the delete button on a specific list item
 
-        handleDeleteClick(index) {
-                // open alert modal
-                NotificationManager.warning('Item Deleted');
-                this.props.dispatch(deleteItem(this.props.title.toLowerCase(), index))
-        };
+  handleDeleteClick(index) {
+    // open alert modal
+    NotificationManager.warning("Item Deleted");
+    this.props.dispatch(deleteItem(this.props.title.toLowerCase(), index));
+  }
 
-        render() {
-                let displayItems;
-                displayItems = this.props.thisState.map((item, index) => {
-                        // for CategoryTotal component
-                        // for creating a unique ref for each input depending on it's index
-                        const name = 'name' + index;
-                        const weight = 'weight' + index;
-                        const importance = 'importance' + index;
-                        console.log(item)
+  render() {
+    let displayItems;
+    displayItems = this.props.thisState.map((item, index) => {
+      // for CategoryTotal component
+      // for creating a unique ref for each input depending on it's index
+      const name = "name" + index;
+      const weight = "weight" + index;
+      const importance = "importance" + index;
+      console.log(item);
 
-                        return (
-                                <fieldset key={index} onChange={() => { this.handleChange({ name: this[name].value, weight: this[weight].value, importance: this[importance].value }, index) }}>
-                                        <div className='fieldParent'>
+      return (
+        <fieldset
+          key={index}
+          onChange={() => {
+            this.handleChange(
+              {
+                name: this[name].value,
+                weight: this[weight].value,
+                importance: this[importance].value
+              },
+              index
+            );
+          }}
+        >
+          <div className="fieldParent">
+            <input
+              className="listInput nameInput"
+              ref={input => {
+                this[name] = input;
+              }}
+              type="text"
+              onClick={e => {
+                e.stopPropagation();
+              }}
+              value={item.name}
+              onChange={() => {
+                this.setState({ change: !this.state.change });
+              }}
+            />
 
-                                                <input className='listInput nameInput' ref={(input) => { this[name] = input }} type='text'
-                                                        onClick={(e) => { e.stopPropagation() }}
-                                                        value={item.name} onChange={() => { this.setState({ change: !this.state.change }) }} />
+            <input
+              className="listInput weightInput"
+              ref={input => {
+                this[weight] = input;
+              }}
+              type="number"
+              onClick={e => {
+                e.stopPropagation();
+              }}
+              value={item.weight}
+              onChange={() => {
+                this.setState({ change: !this.state.change });
+              }}
+            />
 
-                                                <input className='listInput weightInput' ref={(input) => { this[weight] = input }} type='number'
-                                                        onClick={(e) => { e.stopPropagation() }}
-                                                        value={item.weight} onChange={() => { this.setState({ change: !this.state.change }) }} />
+            <select
+              className="listInput selectInput"
+              ref={input => {
+                this[importance] = input;
+              }}
+              onClick={e => {
+                e.stopPropagation();
+              }}
+              value={item.importance}
+              selected={item.importance}
+              onChange={() => {
+                this.setState({ change: !this.state.change });
+              }}
+            >
+              <option value="critical">Critical</option>
+              <option value="important">Important</option>
+              <option value="unimportant">Unimportant</option>
+            </select>
+            <button
+              className="deleteBtn"
+              onClick={e => {
+                e.stopPropagation();
+                e.preventDefault();
+                this.handleDeleteClick(index);
+              }}
+            >
+              <i className="fa fa-trash" aria-hidden="true" />
+            </button>
+          </div>
+        </fieldset>
+      );
+    });
 
-                                                <select className='listInput selectInput'
-                                                        ref={(input) => { this[importance] = input }}
-                                                        onClick={(e) => { e.stopPropagation() }}
-                                                        value={item.importance} selected={item.importance} onChange={() => { this.setState({ change: !this.state.change }) }}>
-                                                        
-                                                        <option value='critical'>Critical</option>
-                                                        <option value='important'>Important</option>
-                                                        <option value='unimportant'>Unimportant</option>
-
-                                                </select>
-                                                <button
-                                                        className='deleteBtn' onClick={(e) => {
-                                                                e.stopPropagation(); e.preventDefault();
-                                                                this.handleDeleteClick(index)
-                                                        }}>
-                                                        <i className="fa fa-trash" aria-hidden="true"></i>
-                                                </button>
-                                        </div>
-                                </fieldset>
-                        )
-                })
-
-                if (this.props.thisState.length === 0) {
-                        displayItems = <div className='emptyListDiv'><div>This list needs some items...</div></div>
-                }
-                return (
-                        <div className='scrollableParent'>
-                                <form className='listItemForm'>
-                                        {displayItems}
-                                </form>
-                        </div>
-                )
-        }
+    if (this.props.thisState.length === 0) {
+      displayItems = (
+        <div className="emptyListDiv">
+          <div>This list needs some items...</div>
+        </div>
+      );
+    }
+    return (
+      <div className="scrollableParent">
+        <form className="listItemForm">
+          <CSSTransitionGroup
+            transitionName="displayItems"
+            transitionEnterTimeout={0}
+            transitionLeaveTimeout={0}
+          >
+            {displayItems}
+          </CSSTransitionGroup>
+        </form>
+      </div>
+    );
+  }
 }
-export default connect()(ListTable)
+export default connect()(ListTable);
