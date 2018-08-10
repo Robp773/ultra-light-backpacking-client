fbfd0056821ec222766d01ffc77e62850de336a0
Robp773 <robpeterman573@gmail.com>
2018-08-10 18:28:23 -0400

    removed 'flashChange' animation

--- src/components/Feedback.js
+++ src/components/Feedback.js
@@ -2,41 +2,27 @@ import React from 'react';
 import './Feedback.css';
 import styled, { keyframes } from 'styled-components';
 
-export default class Feedback extends React.Component{
+export default class Feedback extends React.Component {
 
-render(){
-    const flashChange = keyframes`
-    0% {
-        
-        color: black;
-    }
-    50% {
-      color: #52DEE5;
-    }
-    100% {
-   
-        color: black
-    }
-  `;
-  const expandUnderliners = keyframes`
+    render() {
+
+        const expandUnderliners = keyframes`
          0% {
             border-top: 2px solid black;
             width: 0%;
-        50% { 
-            border-top: 2px solid #52DEE5;
+         }
+
+         50% {
+                border-top: 2px solid #52DEE5;
+         }
+
        100% {
-            border-top: 2px solid black;
+        border-top: 2px solid black;
             width: 100%;
             }
             `;
-  
-  const FlashDiv = styled.div`
-    width: fit-content;
-    margin: auto;
-    animation: 2s ${flashChange} ease-out;
-  `;
 
-  const Underliner = styled.div`
+        const Underliner = styled.div`
   margin-top: 5px;
   margin-bottom: 5px;
   margin-left: auto;
@@ -45,11 +31,12 @@ render(){
   width: 100%;
   animation: 1s ${expandUnderliners};`
 
-return(<FlashDiv className='flashDiv'>
-            <Underliner></Underliner>
+        return (
+            <div id='feedbackParent'>
+                <Underliner></Underliner>
                 {this.props.feedback}
-            <Underliner></Underliner>
-       </FlashDiv>)
+                <Underliner></Underliner>
+            </div>
+        )
     }
 }
-    
