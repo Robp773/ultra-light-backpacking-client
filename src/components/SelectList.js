74f18db6f91f7e1af2e877993c87c89d2bcc7957
Robp773 <robpeterman573@gmail.com>
2018-08-19 16:54:46 -0400

    code reformat

--- src/components/SelectList.js
+++ src/components/SelectList.js
@@ -1,22 +1,18 @@
-
-import React from 'react';
-import './SelectList.css';
-export default class SelectList extends React.Component{
-
-    render(){
-        return (            
-            <div>
-                <header>Pack Light
-                    <img className='backpackingIcon' alt='Backpacker Icon' src='..\images\man-2766763_1280 (1).png'/>
-                </header>
-                <div className='listBody'>
-                    <div className='listNamesParent'>             
-                        <h3 className='selectH3'>Select a List</h3>
-                        {this.props.newListInput}
-                        {this.props.listNames}
-                    </div>
-                </div>
-            </div>
-        )
-    };
-};
+import React from "react";
+import "./SelectList.css";
+export default class SelectList extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="listBody">
+          <div className="listNamesParent">
+            <header>Pack Light</header>
+            <h3 className="selectH3">Select a List</h3>
+            {this.props.newListInput}
+            {this.props.listNames}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
